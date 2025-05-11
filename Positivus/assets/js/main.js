c14fc5cd88925891a382c495c2496326e9c660cd
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-05-11 06:25:18 +0100

    desktop view done

--- Positivus/assets/js/main.js
+++ Positivus/assets/js/main.js
@@ -22,3 +22,23 @@ function toggleAccordion(header) {
 
 
 //TESTIMONIAL CAROUSEL SCRIPT
+let index = 0;
+const slides = document.querySelector('.slides');
+
+function showSlide() {
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function nextSlide() {
+    index = (index + 1) % 3;
+    showSlide();
+}
+
+// Auto-slide every 3 seconds
+setInterval(nextSlide, 5000);
+
+// Allow manual navigation
+function prevSlide() {
+    index = (index - 1 + 3) % 3;
+    showSlide();
+}
